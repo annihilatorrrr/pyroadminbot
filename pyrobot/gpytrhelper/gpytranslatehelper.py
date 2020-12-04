c32412b99aa5d323630c5fdcafbe7abde3794785
iiiiii1wepfj <itaykotik@gmail.com>
2020-12-04 13:40:59 +0200

    Create gpytranslatehelper.py

---
+++ pyrobot/gpytrhelper/gpytranslatehelper.py
@@ -0,0 +1,8 @@
+from pyrogram.types import Message
+from inspect import getfullargspec
+
+
+async def edrep(msg: Message, **kwargs):
+    func = msg.edit_text if msg.from_user.is_self else msg.reply
+    spec = getfullargspec(func.__wrapped__).args
+    await func(**{k: v for k, v in kwargs.items() if k in spec})
