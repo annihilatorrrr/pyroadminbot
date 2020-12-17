02c9f1f56540a95ad2da1e6694da4cfe39fdfe13
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2020-12-17 09:26:41 +0200

    Update translategpy.py

--- pyrobot/plugins/tools/translategpy.py
+++ pyrobot/plugins/tools/translategpy.py
@@ -1,49 +1,37 @@
 from gpytranslate import Translator
-from pyrobot import COMMAND_HAND_LER
 from pyrogram import Client, filters
-from pyrobot.gpytrhelper.gpytranslatehelper import edrep
+from pyrogram.types import Message
+from pyrobot import COMMAND_HAND_LER
 
 
-trl = Translator()
 
-
+gpytrl = Translator()
 
 
 @Client.on_message(filters.command("trg", COMMAND_HAND_LER))
-async def gtranslate(_, message):
-    trl = Translator()
-    if message.reply_to_message and (
-        message.reply_to_message.text or message.reply_to_message.caption
-    ):
-        if len(message.text.split()) == 1:
-            await message.delete()
-            return
-        target = message.text.split()[1]
-        if message.reply_to_message.text:
-            text = message.reply_to_message.text
+async def translate(_, message: Message) -> None:
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        await message.reply_text("Reply to a message to translate it!")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
         else:
-            text = message.reply_to_message.caption
-        try:
-            tekstr = await trl(text, targetlang=target)
-        except ValueError as err:
-            await edrep(message, text=f"Error: `{str(err)}`", parse_mode="Markdown")
-            return
-    else:
-        if len(message.text.split()) <= 2:
-            await message.delete()
-            return
-        target = message.text.split(None, 2)[1]
-        text = message.text.split(None, 2)[2]
-        try:
-            tekstr = await trl(text, targetlang=target)
-        except ValueError as err:
-            await edrep(
-                message, text="Error: `{}`".format(str(err)), parse_mode="Markdown"
-            )
-            return
+            source = await gpytrl.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = await gpytrl.detect(to_translate)
+        dest = "en"
+    translation = await gpytrl(to_translate,
+                              sourcelang=source, targetlang=dest)
+    reply = f"**Translated from** `{source}` **to** `{dest}`:\n```{translation.text}```" 
 
-    await edrep(
-        message,
-        text=f"**Translated from:**```{(await trl.detect(text))}```\n\n**Translated text:**\n\n `{tekstr.text}`",
-        parse_mode="Markdown",
-    )
+    await message.reply_text(reply, parse_mode="markdown")
+
